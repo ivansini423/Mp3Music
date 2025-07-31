e2f59297d300603f64e832122230769ad96f15b0
Jeshua3545926 <garzaromerojeshuaabiram2019ktv@gmail.com>
2025-07-31 16:57:00 -0600

    bd comentada

--- Backend/Bd.js
+++ Backend/Bd.js
@@ -1,15 +1,15 @@
-const connection = async () => {
-    const con = await mysql.createConnection({
-        host: '127.0.0.1',
-        user: 'root',
-        password: 'Jesh',
-        database: 'coffe_dev'
-    });
-    await con.connect();
-    console.log('connected as id ' + con.threadId);
-    return con.promise();
-}
+// const connection = async () => {
+//     const con = await mysql.createConnection({
+//         host: '127.0.0.1',
+//         user: 'root',
+//         password: 'Jesh',
+//         database: 'coffe_dev'
+//     });
+//     await con.connect();
+//     console.log('connected as id ' + con.threadId);
+//     return con.promise();
+// }
 
 
-export default connection;
+// export default connection;
 
