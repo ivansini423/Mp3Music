22ebe966f0310a2812f7e03b55693f77ceab6519
Jeshua3545926 <garzaromerojeshuaabiram2019ktv@gmail.com>
2025-07-31 11:53:17 -0600

    Test

---
+++ Backend/bd.sql
@@ -0,0 +1,5 @@
+use coffe_dev
+
+ALTER TABLE `DataUser` RENAME TO `Users`;
+
+select * from `Users`
---
+++ test/conecction.test.js
@@ -0,0 +1,33 @@
+
+const express = require('express')
+const mysql = require('mysql2/promise')
+
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+const PORT = 3000;
+
+
+
+
+const connection = async () => {
+    const con = await mysql.createConnection({
+        host: '127.0.0.1',
+        user: 'root',
+        password: 'Jesh',
+        database: 'coffe_dev'
+    });
+    await con.connect();
+    console.log('connected as id ' + con.threadId);
+}
+test('conexion a servidor', async () => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+});
+test('conexion a base de datos', async () => { await connection() })
+
